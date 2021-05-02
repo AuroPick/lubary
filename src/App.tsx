6e322a2f7ef0139a37f4a2d186ee48b7a5a8beeb
Aykut Saki <aykutsakisocial@gmail.com>
2021-05-02 23:40:47 +0300

    app update

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,67 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
+import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
+import {
+  DarkTheme as NavigationDarkTheme,
+  DefaultTheme as NavigationDefaultTheme,
+  NavigationContainer
+} from "@react-navigation/native";
+import merge from "deepmerge";
+import * as Font from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import i18n from "i18n-js";
+import React, { useContext, useEffect } from "react";
+import { Alert } from "react-native";
+import {
+  DarkTheme as PaperDarkTheme,
+  DefaultTheme as PaperDefaultTheme,
+  Provider as PaperProvider
+} from "react-native-paper";
+import Toast from "react-native-toast-message";
+import { Context } from "./context";
 import { BottomTabRoutes } from "./routes";
 
-export default function App() {
+const CombinedDefaultTheme = merge(PaperDefaultTheme, NavigationDefaultTheme);
+const CombinedDarkTheme = merge(PaperDarkTheme, NavigationDarkTheme);
+
+export function App() {
+  const { darkTheme, loadTheme, loadLanguage } = useContext(Context);
+
+  useEffect(() => {
+    const preLoad = async () => {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        loadLanguage();
+        loadTheme();
+        await Font.loadAsync({
+          ...MaterialCommunityIcons.font,
+          ...Ionicons.font,
+        });
+      } catch {
+        Alert.alert(
+          i18n.t("settings.notLoaded"),
+          i18n.t("settings.notLoadedSettings"),
+          [
+            { text: i18n.t("settings.cancel"), onPress: () => {} },
+            { text: i18n.t("settings.ok"), onPress: () => {} },
+          ]
+        );
+      } finally {
+        await SplashScreen.hideAsync();
+      }
+    };
+
+    preLoad();
+  }, []);
+
   return (
-    <NavigationContainer>
-      <BottomTabRoutes />
-    </NavigationContainer>
+    <PaperProvider theme={darkTheme ? CombinedDarkTheme : CombinedDefaultTheme}>
+      <NavigationContainer
+        theme={darkTheme ? CombinedDarkTheme : CombinedDefaultTheme}
+      >
+        <BottomTabRoutes />
+        {darkTheme ? <StatusBar style="light" /> : <StatusBar style="dark" />}
+        <Toast ref={(ref) => Toast.setRef(ref)} />
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
